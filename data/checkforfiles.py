31078d02f2a15ca6d4dff49262635961c576a47d
Aron Ambrosiani <aron@ambrosiani.se>
2022-11-21 14:33:23 +0100

    Update checkforfiles.py

--- data/checkforfiles.py
+++ data/checkforfiles.py
@@ -1,11 +1,34 @@
 import csv
 
-with open('files-TIFfromNAS-05.csv', newline='') as f:
-    reader = csv.reader(f)
-    files_list = list(reader)
+file_name_records_list = 'records.csv'
+file_name_files_list = 'files.csv' 
 
-with open('records.csv', newline='') as csvfile:
-	records_csv = csv.reader(csvfile, delimiter=';', quotechar='"')
-	for row in records_csv:
-		if row in files_list:
-			print('found',row)
+ok = []
+files_missing = []
+records_missing = []
+
+with open(file_name_files_list, newline='') as f:
+    files_list = list(csv.reader(f))
+
+
+with open(file_name_records_list, newline='') as f:
+	records_list = list(csv.reader(f))
+
+print ('number of files:', len(files_list))
+print ('number of records:',len(records_list))
+
+
+
+for row in records_list:
+	if row in files_list:
+		ok.append(row[0])
+	else:
+		files_missing.append(row[0])
+
+for row in files_list:
+	if row not in records_list:
+		records_missing.append(row[0])
+
+print ('record & file ok:',len(ok), '\n')
+print ('file missing:', len(files_missing),*files_missing, '\n', sep='\n')
+print ('record missing:', len(records_missing),*records_missing, '\n', sep='\n')
